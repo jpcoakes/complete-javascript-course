75cfe326fffb8ad6775f901d604ca0ca82941a41
Joel Coakes <jpcoakes@gmail.com>
2023-09-18 22:38:17 -0400

    Completed to section 42 from Fundamentals part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -43,10 +43,199 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1991);
 console.log(age2);
-*/
 
 // Arrow function
 
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1988, 'Joel'));
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    }
+    else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+    // return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+
+/* FUNCTION REVIEW
+
+// Function Declaration
+// can be used before it is declared
+function calcAge(birthYear) {
+    return 2037 - birthYear;
+}
+
+// Function expression
+// essentially a function value stored in a variable
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+// Arrow function
+// great for quick one-line functions
+// has no this keyword
+const calcAge = birthYear => 2037 - birthYear;
+
+
+// Challenge #1
+/* Write your code below. Good luck! 🙂 */
+/*
+const calcAverage = (score1, score2, score3) => {
+    return (score1 + score2 + score3) / 3;
+}
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+function checkWinner (avgDolphins, avgKoalas) {
+    if (avgDolphins > (avgKoalas * 2)){
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    }
+    else if (avgKoalas > (avgDolphins *2)){
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    }
+    else {
+        console.log('No team wins...');
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice'];
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// adding elements. push and unshift also can return the new length of the array
+// add element to end
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+// add element to beginning
+friends.unshift('John');
+console.log(friends);
+
+// remove element with pop, and also can return the element removed
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+// remove first element with shift
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23')); // this is false because it is a strict comparison 23 Number is different than 23 String
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
+
+// Challenge #2
+
+function calcTip (bill) {
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill *.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [calcTip(bills[0]) + bills[0], calcTip(bills[1]) + bills[1], calcTip(bills[2]) + bills[2]];
+
+*/
+
+// BIG LEARNING EVENT with difference between Arrays and Objects in Javascript
+// Arrays just have the values, but Objects have key - value pairs
+
+// arrays get square brackets
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+// objects get curly brackets
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
